4f728deaecd7d8d5d265cfed9530288d04887cb8
KaengEE <yopuiqwer@naver.com>
2024-01-22 11:35:24 +0900

    카테고리 리스트 출력

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -1,53 +1,54 @@
-import React from "react";
-import { Button, Card } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Card } from "react-bootstrap";
 import img from "../../assets/sample.jpg";
 import { Link } from "react-router-dom";
+import categoryService from "../../services/category.service";
 
 const Category = () => {
+  const [category, setCategory] = useState([]); //카테고리 리스트
+
+  useEffect(() => {
+    // 컴포넌트가 마운트되면 카테고리 리스트를 불러오기
+    const categoryList = async () => {
+      try {
+        const response = await categoryService.getList();
+        setCategory(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("리스트를 가져오는데 오류 발생:", error);
+      }
+    };
+
+    categoryList();
+  }, []);
+
   return (
     <div className="container mt-4">
       <h2>카테고리</h2>
+      {/* ROLE ADMIN에게만 보이게하기 */}
+      <div className="text-end mx-3">
+        <Link to="/create" className="btn btn-primary mb-3">
+          카테고리 추가
+        </Link>
+      </div>
       <div className="d-flex flex-wrap gap-3 mt-3">
-        {/* 카테고리 테이블 데이터 출력 */}
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src={img} />
-          <Card.Body>
-            <Card.Title>그림</Card.Title>
-            <div className="text-end">
-              <Link to="/teamList" className="btn btn-primary">
-                바로가기
-              </Link>
-            </div>
-          </Card.Body>
-        </Card>
-        {/* 반복문으로처리 */}
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src={img} />
-          <Card.Body>
-            <Card.Title>그림</Card.Title>
-            <div className="text-end">
-              <Button variant="primary">바로가기</Button>
-            </div>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src={img} />
-          <Card.Body>
-            <Card.Title>그림</Card.Title>
-            <div className="text-end">
-              <Button variant="primary">바로가기</Button>
-            </div>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src={img} />
-          <Card.Body>
-            <Card.Title>그림</Card.Title>
-            <div className="text-end">
-              <Button variant="primary">바로가기</Button>
-            </div>
-          </Card.Body>
-        </Card>
+        {/* 카테고리 리스트 */}
+        {category.map((categoryItem) => (
+          <Card key={categoryItem.id} style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>{categoryItem.category}</Card.Title>
+              <div className="text-end">
+                {/* 링크 또는 버튼에 대한 작업 수행 */}
+                <Link
+                  to={`/team/${categoryItem.id}`}
+                  className="btn btn-primary"
+                >
+                  바로가기
+                </Link>
+              </div>
+            </Card.Body>
+          </Card>
+        ))}
       </div>
     </div>
   );
---
+++ src/services/category.service.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { BASE_API_URL } from "../common/constants";
+import { authHeader } from "./base.service";
+
+const API_URL = BASE_API_URL + "/api/category";
+
+class CategoryService {
+  //카테고리 리스트 불러오기
+  getList() {
+    return axios.get(`${API_URL}/list`, { headers: authHeader() });
+  }
+}
+
+const categoryService = new CategoryService();
+
+export default categoryService;
