c10d143048716a1baef5666ece2ced8e42a7c893
KaengEE <yopuiqwer@naver.com>
2024-01-24 12:26:13 +0900

    팀리스트8개가져오기

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,35 +1,35 @@
 import React, { useEffect, useState } from "react";
 import banner from "../../assets/banner.gif";
-import "./Home.css"
+import "./Home.css";
 import { Link } from "react-router-dom";
 import teamService from "../../services/team.service";
 import { Button, Card, Col, Row } from "react-bootstrap";
 
 const Home = () => {
-  const [teamList, setTeamList] = useState([])
+  const [teamList, setTeamList] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchTeamList();
-  },[])
+  }, []);
 
   //팀리스트 가져오기
   const fetchTeamList = async () => {
     try {
       const response = await teamService.getTeamList();
-      setTeamList(response.data);
-      //console.log(response.data);
-     // console.log(teamList);
+      const latestTeams = response.data.slice(0, 8); //8개가져오기
+      setTeamList(latestTeams);
     } catch (error) {
       console.error("팀 리스트를 가져오는데 오류 발생:", error);
     }
   };
 
-
   return (
     <div className="container text-center mt-1">
       <div className="position-relative">
         <img src={banner} alt="Banner" className="banner-image" />
-        <Link to="/category" className="btn btn-on-image">팀 만들기</Link>
+        <Link to="/category" className="btn btn-on-image">
+          팀 만들기
+        </Link>
       </div>
       {/* 팀리스트 출력 */}
       <div className="mt-4">
@@ -39,7 +39,11 @@ const Home = () => {
             {teamList.map((team) => (
               <Col key={team.id} xs={12} sm={6} md={4} lg={3}>
                 <Card className="mb-3">
-                  <Card.Img variant="top" src={team.teamImg} alt={team.teamname} />
+                  <Card.Img
+                    variant="top"
+                    src={team.teamImg}
+                    alt={team.teamname}
+                  />
                   <Card.Body>
                     <Card.Title>{team.teamname}</Card.Title>
                     <Card.Text>{team.teamTitle}</Card.Text>
