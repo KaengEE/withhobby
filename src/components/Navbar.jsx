d4d0becc38e4fa9a38c3aae66bb008ccd83d3504
KaengEE <yopuiqwer@naver.com>
2024-01-24 12:26:20 +0900

    아이콘 주소설정

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand" href="/">
           <img
             src="/vite.svg"
             alt="Logo"
