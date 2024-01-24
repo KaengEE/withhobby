5f793492272020d62e305f7880d894d9a0e10b0e
KaengEE <yopuiqwer@naver.com>
2024-01-24 15:12:42 +0900

    StrictMode 삭제

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,7 @@ import { Provider } from "react-redux";
 import store from "./store/configStore";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
